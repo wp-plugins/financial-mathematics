83284451e97cd0f2ce8eb20b79094bbce144c804
owenks <owenks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-10 17:47:09 +0000

    ct1 interface reduced
    git-svn-id: https://plugins.svn.wordpress.org/financial-mathematics/trunk@711029 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- interface-ct1-concept.php
+++ interface-ct1-concept.php
@@ -5,5 +5,4 @@ interface CT1_Concept{
 	public function get_calculator( $parameters );
 	public function get_solution( $parameters );
 	public function set_score( $parameters );
-	public function equals( $parameters );
 }
