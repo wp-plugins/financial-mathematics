2753ad122167a14f60401e477f708b446a6173ce
owenks <owenks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-05 11:41:44 +0000

    tag 1.13 annuity symbol + bug fix
    git-svn-id: https://plugins.svn.wordpress.org/financial-mathematics/tags/1.13@723034 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php   
+
+
+$path_to_pear = '/big/dom/xbdmfst/owenks/pear/share/pear/';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path_to_pear);
+
+function CT1_autoloader($class, $file){
+	if (!class_exists($class)){
+		if (!include($file)){ 
+			throw new Exception("Can't instantiate " . $class . " in " . __FILE__ );
+		}
+		else{
+			require_once ($file);
+			if (!class_exists($class)){
+				throw new Exception("Can't instantiate " . $class . " in " . __FILE__ );
+			}
+		}
+	}
+}
+
